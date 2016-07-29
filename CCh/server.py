b007e39fb6cdda587e87e6fd9bc6e1ffdcd2d1fc
bnddnb <bndong@uwaterloo.ca>
2016-07-29 15:15:16 -0400

    2nd commit

---
+++ CCh/server.py
@@ -0,0 +1,78 @@
+import socket
+import sys
+from thread import *
+
+'''
+Defalt & setting on host&port
+'''
+# Symbolic name meaning all available interfaces
+try:
+    HOST = input("Please enter host address:")
+except SyntaxError:
+    HOST = None
+   
+# Arbitrary non-privileged port
+try:
+    PORT = int(input("Port number:"))
+except SyntaxError:
+    PORT = None
+
+#default value for PORT and HOST
+
+HOST = 'localhost' if HOST is None else HOST
+PORT = 8000 if PORT is None else PORT
+
+####################################################################
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#Start listening on socket
+s.listen(5)
+print 'Socket now listening'
+
+ 
+###################################################################
+  
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+    #Sending message to connected client
+    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+     
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:
+         
+        #Receiving from client
+        data = conn.recv(1024)
+        reply = 'OK...' + data
+        if not data: 
+            break
+     
+        conn.sendall(reply)
+     
+    #came out of loop
+    conn.close()
+ 
+#####################################################################
+
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+     
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+ 
+s.close()
